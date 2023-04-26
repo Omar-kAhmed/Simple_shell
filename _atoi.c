c89bb5001c7031b8a073d028c6cfee7f1f43ba67
Omar K. Ahmed <111398535+Omar-kAhmed@users.noreply.github.com>
2023-04-27 01:01:08 +0200

    Update _atoi.c

--- _atoi.c
+++ _atoi.c
@@ -1,42 +1,37 @@
 #include "shell.h"
 
 /**
- * interactive - returns true if shell is interactive mode
- * @info: struct address
- *
+ * interactive - returns true if shell is in interactive mode
+ * @info: pointer to struct info_t
  * Return: 1 if interactive mode, 0 otherwise
  */
 int interactive(info_t *info)
 {
-	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+	return isatty(STDIN_FILENO) && info->readfd <= STDERR_FILENO;
 }
 
 /**
- * is_delim - checks if character is a delimeter
- * @c: the char to check
- * @delim: the delimeter string
- * Return: 1 if true, 0 if false
+ * is_delim - checks if character is a delimiter
+ * @c: the character to check
+ * @delim: the delimiter string
+ * Return: 1 if true, 0 otherwise
  */
-int is_delim(char c, char *delim)
+int is_delim(char c, const char *delim)
 {
 	while (*delim)
 		if (*delim++ == c)
-			return (1);
-	return (0);
+			return 1;
+	return 0;
 }
 
 /**
  * _isalpha - checks for alphabetic character
- * @c: The character to input
+ * @c: The character to check
  * Return: 1 if c is alphabetic, 0 otherwise
  */
-
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-		return (1);
-	else
-		return (0);
+	return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
 }
 
 /**
@@ -44,22 +39,20 @@ int _isalpha(int c)
  * @s: the string to be converted
  * Return: 0 if no numbers in string, converted number otherwise
  */
-
-int _atoi(char *s)
+int _atoi(const char *s)
 {
-	int i, sign = 1, flag = 0, output;
+	int sign = 1, output = 0;
 	unsigned int result = 0;
+	int flag = 0;
 
-	for (i = 0; s[i] != '\0' && flag != 2; i++)
+	for (; *s && flag != 2; s++)
 	{
-		if (s[i] == '-')
-			sign *= -1;
-
-		if (s[i] >= '0' && s[i] <= '9')
+		if (*s == '-')
+			sign = -sign;
+		else if (*s >= '0' && *s <= '9')
 		{
 			flag = 1;
-			result *= 10;
-			result += (s[i] - '0');
+			result = result * 10 + (*s - '0');
 		}
 		else if (flag == 1)
 			flag = 2;
@@ -70,6 +63,5 @@ int _atoi(char *s)
 	else
 		output = result;
 
-	return (output);
+	return output;
 }
-
